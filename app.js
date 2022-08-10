045a23fca2e968bba8d1e366476a255c18e25a79
Marty Nelson <marty@martypdx.com>
2022-08-09 18:53:45 -0700

    more examples

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 // import functions and grab DOM elements
-const heading = document.getElementById('heading');
 const nameInput = document.getElementById('name');
-
+const animalSelect = document.getElementById('animal-select');
+const nameDisplay = document.getElementById('name-display');
+const animalDisplay = document.getElementById('animal-display');
 // let state
 
 // set event listeners
 nameInput.addEventListener('input', () => {
     // get user input state
     // update DOM to reflect the new state
-    heading.textContent = nameInput.value;
+    nameDisplay.textContent = nameInput.value;
+});
+
+animalSelect.addEventListener('change', () => {
+    // get user input state
+    // update DOM to reflect the new state
+    animalDisplay.textContent = animalSelect.value;
 });
